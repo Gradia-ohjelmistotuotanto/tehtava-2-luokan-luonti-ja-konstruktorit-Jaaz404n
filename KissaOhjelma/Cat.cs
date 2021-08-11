5da3610d6dde780b6995fb338dcc9d1ca7c4ac0f
Jaaz404n <82179872+Jaaz404n@users.noreply.github.com>
2021-08-11 13:35:54 +0300

    Kissa: Luokan luominen

---
+++ KissaOhjelma/Cat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KissaOhjelma
+{
+    class Cat
+    {
+        private int age;
+        public string name;
+
+        public Cat() {}
+
+        public Cat(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+    }
+}
---
+++ KissaOhjelma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KissaOhjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat Kissa01 = new Cat();
+            Kissa01.name = "Miuku";
+            Console.WriteLine("Kissa01 name: " + Kissa01.GetName());
+            Kissa01.SetName("Mauku");
+            Console.WriteLine("Kissa01 name: " + Kissa01.GetName());
+            Console.WriteLine("Kissa01 age: " + Kissa01.GetAge());
+
+            Cat Kissa02 = new Cat(13, "Mouru");
+            Console.WriteLine("Kissa02 name: " + Kissa02.GetName());
+            Console.WriteLine("Kissa02 age: " + Kissa02.GetAge());
+        }
+    }
+}
